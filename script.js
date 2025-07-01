54b72d2c2009c66e415c30a09807f6c36d75a3a1
Matthew <motmydro.2@gmail.com>
2025-07-01 18:58:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// document.querySelectorAll('.progress-bar').forEach(bar => {
+//     const fillValue = bar.getAttribute('data-fill');
+//     bar.computedStyleMap.setProperty('--fill', fillValue);
+// })
